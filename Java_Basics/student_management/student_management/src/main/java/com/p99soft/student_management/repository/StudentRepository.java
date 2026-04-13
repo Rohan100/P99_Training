859aa305b5730de1e24ffd1542fefe398a9c9738
Rohan100 <rohannagare8355@gmail.com>
2026-04-13 09:15:38 +0530

    java Basics

--- Java_Basics/student_management/student_management/src/main/java/com/p99soft/student_management/repository/StudentRepository.java
+++ Java_Basics/student_management/student_management/src/main/java/com/p99soft/student_management/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package com.p99soft.student_management.repository;
 
-public class StudentRepository {
+import com.p99soft.student_management.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+
+
 }
--- Java_Basics/student_management/student_management/src/main/java/com/p99soft/student_management/service/StudentService.java
+++ Java_Basics/student_management/student_management/src/main/java/com/p99soft/student_management/service/StudentService.java
@@ -1,4 +1,45 @@
-package com.p99soft.student_management.sevice;
+package com.p99soft.student_management.service;
 
+import com.p99soft.student_management.entity.Student;
+import com.p99soft.student_management.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Student createStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Optional<Student> getStudentById(Long id) {
+        return studentRepository.findById(id);
+    }
+
+    public Student updateStudent(Long id, Student updatedStudent) {
+
+        Student student = studentRepository.findById(id).orElseThrow();
+
+        student.setName(updatedStudent.getName());
+        student.setAge(updatedStudent.getAge());
+        student.setCourse(updatedStudent.getCourse());
+
+        return studentRepository.save(student);
+    }
+
+    public void deleteStudent(Long id) {
+        studentRepository.deleteById(id);
+    }
 }
